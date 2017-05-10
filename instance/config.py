3e85a902befdd87abd275abca92e26f798ffb5c1
rndviktor2devman <7irb291wqkmu@mail.ru>
2017-05-10 22:50:26 +0300

    environment

--- instance/config.py
+++ instance/config.py
@@ -5,7 +5,4 @@ DEBUG = os.environ.get('SERVER_DEBUG_MODE')
 DEVELOPMENT = os.environ.get('SERVER_DEVELOPMENT_MODE')
 TESTING = False
 CSRF_ENABLED = True
-SECRET_KEY = 'top  secret!'
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
--- server.py
+++ server.py
@@ -1,9 +1,25 @@
 from flask import Flask, render_template
+from datetime import datetime, date
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
+
 
 app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+db.reflect()
+
+
+class Orders(db.Model):
+    __table__ = db.Model.metadata.tables['orders']
+
 
 @app.route('/')
 def score():
+    unfinished_order = Orders.query.order_by(Orders.created).filter(Orders.confirmed.is_(None)).first()
+    wait_seconds = round((datetime.now() - unfinished_order.created).total_seconds())
+    unfinished_amount = Orders.query.filter(Orders.confirmed.is_(None)).count()
+    
     return render_template('score.html')
 
 if __name__ == "__main__":
