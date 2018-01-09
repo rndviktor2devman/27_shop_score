2fde6867983d5791c8d838967553c1cbd95f5d9b
rndviktor2devman <7irb291wqkmu@mail.ru>
2018-01-10 00:17:38 +0300

    temp refactoring

--- server.py
+++ server.py
@@ -14,49 +14,33 @@ class Orders(db.Model):
     __table__ = db.Model.metadata.tables['orders']
 
 
-@app.route('/get_status')
-def status():
-    today_orders = Orders.query.filter(cast(Orders.created, Date) == datetime.now().date())
-    orders_confirmed = []
-    orders_waiting = []
-    for order in today_orders.all():
-        if order.confirmed is None:
-            wait_time = (datetime.now() - order.created).total_seconds() / 60
-            confirmed = None
-        else:
-            wait_time = (order.confirmed - order.created).total_seconds() / 60
-            confirmed = order.confirmed.strftime("%H:%M:%S")
-        if wait_time < 7:
-            wait_status = 'color-green'
-        elif wait_time < 30:
-            wait_status = 'color-yellow'
-        else:
-            wait_status = 'color-red'
-        order_data = {
-            'price': str(order.price),
-            'status': order.status,
-            'created': order.created.strftime("%H:%M:%S"),
-            'confirmed': confirmed,
-            'wait_status': wait_status
-        }
-        if order_data['confirmed'] is not None:
-            orders_confirmed.append(order_data)
-        else:
-            orders_waiting.append(order_data)
-
-    orders_confirmed.sort(key=lambda x: x['created'])
-    orders_waiting.sort(key=lambda x: x['created'])
-
-    response_data = {
-        'orders_confirmed': orders_confirmed,
-        'orders_waiting': orders_waiting
-    }
-    return jsonify(response_data)
+def get_waiting_time():
+    order = Orders.query.filter(
+        Orders.confirmed.is_(None)).first()
+    if order is None:
+        return 0
+    else:
+        return datetime.now().hour*60 + datetime.now().minute -\
+               order.created.hour+order.created.minute
 
 
 @app.route('/')
 def score():
-    return render_template('score.html')
+    confirmed_orders = Orders.query.filter(
+        Orders.confirmed.isnot(None)).order_by(Orders.status)
+    today_date = datetime.now()
+    today_confirmed = [order for order in confirmed_orders
+                       if order.confirmed.day == today_date.day
+                       and order.confirmed.month == today_date.month]
+    unconfirmed_orders = Orders.query.filter(
+        Orders.confirmed.is_(None)).order_by(Orders.status).count()
+
+    wait_time = get_waiting_time()
+    return render_template('score.html',
+                           processed_day=len(today_confirmed),
+                           waiting_time=wait_time,
+                           unconfirmed=unconfirmed_orders)
+
 
 if __name__ == "__main__":
     app.run()
