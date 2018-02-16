1589b3d132923b3a3ff4f30ffce62e39ee2bbe9d
rndviktor2devman <7irb291wqkmu@mail.ru>
2018-02-16 21:51:13 +0300

    orders separated from server

--- server.py
+++ server.py
@@ -1,24 +1,8 @@
 from flask import Flask, render_template, request, jsonify, \
     send_from_directory
 from datetime import datetime, timedelta, date
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, String, DateTime, Enum, and_
-from os import getenv
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-
-class Orders(db.Model):
-    __tablename__ = 'orders'
-
-    id = db.Column(String, primary_key=True)
-    created = db.Column(DateTime, nullable=False, index=True)
-    confirmed = db.Column(DateTime, index=True)
-    status = db.Column(Enum('DRAFT', 'FULFILLMENT', 'CANCELED', 'COMPLETED',
-                            name='statuses'), nullable=False, index=True)
+from sqlalchemy import and_
+from orders import Orders, app
 
 
 def get_waiting_time():
