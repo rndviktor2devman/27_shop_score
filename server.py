314a9ef6cc7c3e96f9d311ffab4e85f7b43c3550
rndviktor2devman <7irb291wqkmu@mail.ru>
2018-02-16 22:13:47 +0300

    formatting fixed

--- server.py
+++ server.py
@@ -1,17 +1,17 @@
-from flask import Flask, render_template, request, jsonify, \
-    send_from_directory
+from flask import render_template, request, jsonify, send_from_directory
 from datetime import datetime, timedelta, date
 from sqlalchemy import and_
 from orders import Orders, app
 
 
-def get_waiting_time():
-    order = Orders.query.filter(Orders.created > date.today(),
-                                and_(Orders.confirmed.is_(None))).first()
-    if order is None:
+def get_older_order_waiting_time():
+    older_order = Orders.query.filter(
+        Orders.created > date.today(),
+        and_(Orders.confirmed.is_(None))).order_by(Orders.created).first()
+    if older_order is None:
         return 0
     else:
-        wait_time = datetime.now() - order.created
+        wait_time = datetime.now() - older_order.created
         return wait_time - timedelta(microseconds=wait_time.microseconds)
 
 
@@ -22,14 +22,14 @@ def robots_file():
 
 @app.route('/check_state')
 def check_state():
-    today_confirmed = Orders.query.filter(Orders.confirmed.isnot(None),
-                                          and_(Orders.confirmed > date.today())
-                                          ).order_by(Orders.status).all()
+    today_confirmed = Orders.query.filter(
+        Orders.confirmed.isnot(None),
+        and_(Orders.confirmed > date.today())).order_by(Orders.status).all()
 
     unconfirmed_orders = Orders.query.filter(
         Orders.confirmed.is_(None)).order_by(Orders.status).count()
 
-    wait_time = get_waiting_time()
+    wait_time = get_older_order_waiting_time()
     return jsonify(processed_day=len(today_confirmed),
                    waiting_time=wait_time,
                    unconfirmed=unconfirmed_orders)
